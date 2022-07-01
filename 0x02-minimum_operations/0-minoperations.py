9ba9c48ca2e4cc2a3e39b887b8904c181d0b75d6
zyambo <mightypz2@gmail.com>
2022-07-01 05:50:20 +0000

    handle exceptions

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -14,15 +14,18 @@ def minOperations(n: int) -> int:
     """
 
     def dfs(n: int):
-        if n == 1 or n > 10000:
+        try:
+            if n == 1:
+                return 0
+            for i in range(n // 2, 0, -1):
+                if n % i == 0:
+                    return 1 + ((n // i) - 1) + dfs(i)  # copy, paste
+                    # Here 1 is for copy the all previous, for example,
+                    # n is 21, i is 7.
+                    # We used 1 step to copy all 7's
+                    # ((n // i) - 1) is (21 // 7) - 1. Here we have 7 already,
+                    # used 2 steps to paste 7's
+                    # To get 7's on the notepad, we need dfs(7) steps.
+        except Exception:
             return 0
-        for i in range(n // 2, 0, -1):
-            if n % i == 0:
-                return 1 + ((n // i) - 1) + dfs(i)  # copy, paste
-                # Here 1 is for copy the all previous, for example,
-                # n is 21, i is 7.
-                # We used 1 step to copy all 7's
-                # ((n // i) - 1) is (21 // 7) - 1. Here we have 7 already,
-                # used 2 steps to paste 7's
-                # To get 7's on the notepad, we need dfs(7) steps.
     return dfs(n)
