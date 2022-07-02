303ab309538ffc296bcc59d0e75cc60bf25c5d36
zyambo <mightypz2@gmail.com>
2022-07-02 10:04:13 +0200

    [FIX] change algorithm

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -10,23 +10,23 @@ import math
 def minOperations(n: int) -> int:
     """
         calculates the fewest number of operations needed to result
-        in exactly n H characters in the file. and returns
-        an integer, if n is impossible to reach, return 0
-    """
+    """ 
 
-    def dfs(n: int):
-        try:
-            if n == 1 or math.isinf(n):
-                return 0
-            for i in range(n // 2, 0, -1):
-                if n % i == 0:
-                    return 1 + ((n // i) - 1) + dfs(i)  # copy, paste
-                    # Here 1 is for copy the all previous, for example,
-                    # n is 21, i is 7.
-                    # We used 1 step to copy all 7's
-                    # ((n // i) - 1) is (21 // 7) - 1. Here we have 7 already,
-                    # used 2 steps to paste 7's
-                    # To get 7's on the notepad, we need dfs(7) steps.
-        except MemoryError:
-            return 0
-    return dfs(n)
+    if type(n) == int or n > 1:
+        return int(ops(n))
+    else:
+        return 0
+
+
+def ops(n: int) -> int:
+    """ calculate n operations to copy paste"""
+    op = 2
+    sum = 0
+
+    while n >= op:
+        if n % op == 0:
+            n = n // op
+            sum += op
+        else:
+            op += 1
+    return sum
