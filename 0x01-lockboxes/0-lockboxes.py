74d8f276e8bc5d67e505ffef2868451b408e6edc
zyambo <mightypz2@gmail.com>
2022-07-02 14:25:34 +0200

    complete task

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Write a method to determine if boxes can be opened"""
+from typing import List
+
+
+def canUnlockAll(boxes: List) -> bool:
+    """ Chech if box is openable"""
+
+    if len(boxes) < 1:
+        return False
+    box = [0]
+    for keys in box:
+        for key in boxes[keys]:
+            if key not in box:
+                if key < len(boxes):
+                    box.append(key)
+    if len(box) == len(boxes):
+        return True
+    return False
+
