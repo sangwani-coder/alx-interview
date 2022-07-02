dcf2a34dd8e8b64e138953f760d333092b303143
zyambo <mightypz2@gmail.com>
2022-07-02 02:00:26 +0200

    [FIX] Modify loop nesting

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -9,17 +9,14 @@ def pascal_triangle(n: int) -> list:
     """
 
     iList = []
-    if n <= 0:
-        return iList  # Empty list
+    for i in range(1, n + 1):
+        row = []
+        for j in range(i):
+            if j == 0 or j == i - 1:
+                row.append(1)
+            else:
+                n = iList[i - 2][j - 1] + iList[i -2][j]
+                row.append(n)
+        iList.append(row)
 
-    for i in range(n):
-        iList.append([])
-        iList[i].append(1)  # append 1 as first number in each list
-
-        for j in range(1, i):
-            iList[i].append(iList[i-1][j-1]+iList[i-1][j])
-        if(n != 0):
-            if i == 0:  # skip adding another 1 to first list in iList
-                continue
-            iList[i].append(1)  # Append 1 as last number in each list
     return iList
