0def6c61c6bfd84cf815879591248eb69f62879e
zyambo <mightypz2@gmail.com>
2022-07-02 02:23:32 +0200

    Revert "Go back to first debug"
    This reverts commit 0a374af9b65bb5558e85fc8399e3a4051c7e4d96.

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -9,14 +9,13 @@ def pascal_triangle(n: int) -> list:
     """
 
     iList = []
-    for i in range(1, n + 1):
-        row = []
-        for j in range(i):
-            if j == 0 or j == i - 1:
-                row.append(1)
-            else:
-                n = iList[i - 2][j - 1] + iList[i -2][j]
-                row.append(n)
-        iList.append(row)
+    for i in range(n):
+        iList.append([])
+        iList[i].append(1)
 
+        for j in range(1, i):
+            iList[i].append(iList[i-1][j-1]+iList[i-1][j])
+        if i == 0 :
+            continue
+        iList[i].append(1)
     return iList
