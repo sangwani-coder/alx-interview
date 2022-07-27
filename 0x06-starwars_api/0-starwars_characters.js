a4f1801def794bee30bcefcfd3ccd5612b03d5f9
zyambo <mightypz2@gmail.com>
2022-07-27 20:42:17 +0200

    [Debug] create printSorted function

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,20 +1,35 @@
 #!/usr/bin/node
 
-const filmId = process.argv[2];
 const request = require('request');
-
+const { exit } = require('process');
+const filmId = process.argv[2];
 const api = `https://swapi-api.hbtn.io/api/films/${filmId}/`;
 
-request(api, (error, response, body) => {
-  if (error) console.log(error);
-  const data = JSON.parse(body);
-  const chars = data.characters;
+if (filmId === undefined) {
+  console.log('Usage: node 0-starwars_characters.js <film id>');
+  exit();
+}
 
-  for (let i = 0; i < chars.length; i++) {
-    request(chars[i], (error, response, actors) => {
-      if (error) console.log(error);
-      const actor = JSON.parse(actors);
-      console.log(actor.name);
-    });
+// GET film data
+request(api, (error, response, body) => {
+  if (error) {
+    console.log('error', error);
+  } else {
+    const data = JSON.parse(body).characters;
+    printSorted(data, 0);
   }
 });
+
+// GET each character in sorted order
+function printSorted (chars, n) {
+  if (n === chars.length) { return; }
+  request(chars[n], (error, response, actors) => {
+    if (error) {
+      console.log('error', error);
+    } else {
+      const actor = JSON.parse(actors);
+      console.log(actor.name);
+      printSorted(chars, n + 1);
+    }
+  });
+}
