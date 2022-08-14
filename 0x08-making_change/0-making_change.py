bff5b9ea5e9ecc9c989e79b9c9c0fac90e6a2f17
zyambo <mightypz2@gmail.com>
2022-08-14 12:53:03 +0200

    [FIX] table initialization

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -4,29 +4,30 @@
     total: the change to be given
     return: fewest number of coins needed to meet total, otherwise return -1
 """
-import sys
+#import sys
 
 
 def makeChange(coins: list, total: int) -> int:
     """ find min coins to make change"""
     if total == 0:
         return 0
+    MAX = total + 1
     size: int = len(coins)
-    change: int = [0] * len(total + 1)
+    change: int = [0] * (total + 1)
     change[0] = 0
     # initialize change values as infinite
     for i in range(1, total + 1):
-        change[i] = sys.maxsize
+        change[i] = MAX
     # find min coins required from 1 to total
     for i in range(1, total + 1):
         # go through all coins less than i
         for j in range(size):
             if coins[j] <= i:
                 count: int = change[i - coins[j]]
-                if count != sys.maxsize and count + 1 < change[i]:
+                if count != MAX and count + 1 < change[i]:
                     change[i] = count + 1
 
-    if change[total] == sys.maxsize:
+    if change[total] == MAX:
         return -1
     return change[total]
 
--- 0x08-making_change/main.py
+++ 0x08-making_change/main.py
@@ -3,7 +3,7 @@
 Main file for testing
 """
 
-makeChange = __import__('1-making_change').makeChange
+makeChange = __import__('0-making_change').makeChange
 
 print(makeChange([1, 2, 25], 37))
 
