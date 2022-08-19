21aa1f554b69cd6c768f8834a82d3664babb1666
zyambo <mightypz2@gmail.com>
2022-08-19 11:30:15 +0000

    [FIX] remove trailling white space

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -15,7 +15,7 @@ def pascal_triangle(n: int) -> list:
 
         for j in range(1, i):
             iList[i].append(iList[i-1][j-1]+iList[i-1][j])
-        if i == 0 :
+        if i == 0:
             continue
         iList[i].append(1)
     return iList
